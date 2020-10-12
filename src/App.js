25b251478bfead8c99794f7168d3e443b96207b4
rashmiranganath <rashmi18@navgurukul.org>
2020-10-12 21:59:54 +0530

    github finder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import GithubFinder from './components/githubFinder'
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <GithubFinder/>
     </div>
   );
--- src/components/githubFinder.js
+++ src/components/githubFinder.js
@@ -2,10 +2,11 @@ import React from "react";
 import axios from "axios";
 import "./githubFinder.css";
 
-class GithubFinder extends React.Component {
+class GithubFinder extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
+      dropDownValue: "",
       username: "",
       data: [],
       isLoaded: false,
@@ -14,11 +15,11 @@ class GithubFinder extends React.Component {
 
   updateInput(input) {
     this.setState({ username: input });
-    // console.log(input)
   }
 
-  apiHit(username) {
-    // console.log(username)
+  searchUserApi(username) {
+    console.log(this.state);
+    console.log(username);
     axios
       .get(`https://api.github.com/search/users?q=${username}`)
       .then((response) => {
@@ -33,26 +34,68 @@ class GithubFinder extends React.Component {
       });
   }
 
-  render() {
+  dropDownChangeValue(event) {
+    this.setState({
+      dropDownValue: event,
+    });
+  }
+
+  handleSubmit = (event) => {
     const { data } = this.state;
+    event.preventDefault();
+    switch (this.state.dropDownValue) {
+      case "Ascending":
+        var sorted = data.sort((a , b) => {return a.login  - b.login})
+        console.log("a")
+        console.log(sorted)
+        console.log(this.state.dropDownValue)
+        this.setState({
+          data : sorted
+        })
+        break;
+      case "Descending":
+        var sorted = data.sort((a , b) => {return b.login - a.login})
+        console.log("d")
+        this.setState({
+          data : sorted
+        })
+        break;
+
+    }
+  };
+
+  render() {
+    const { data, username, dropDownValue } = this.state;
+    console.log(this.state);
     return (
       <div className="navBar">
         <div className="inputBox">
-          <div className="githubIcon">
-            <i class="fa fa-github"></i>
+          <div className="sortBox">
+            <form onSubmit={this.handleSubmit}>
+              <select
+                name="sort"
+                value={dropDownValue}
+                onChange={(event) => {
+                  this.dropDownChangeValue(event.target.value);
+                }}
+              >
+                <option value="Ascending">Ascending</option>
+                <option value="Descending">Descending</option>
+              </select>
+              <input type="submit" value="Submit" />
+            </form>
           </div>
           <div className="searchBox">
             <input
               type="text"
               required
+              placeholder="username"
               value={this.state.username}
               onChange={(e) => this.updateInput(e.target.value)}
-            />
+            ></input>
           </div>
           <div className="submitBox">
-            <button onClick={() => this.apiHit(this.state.username)}>
-              search
-            </button>
+            <button onClick={() => this.searchUserApi(username)}>search</button>
           </div>
         </div>
         <div className="users">
@@ -63,8 +106,10 @@ class GithubFinder extends React.Component {
                   <img src={item.avatar_url}></img>
                   <div className="userDetails">
                     <h3>{item.login}</h3>
-                    <button className="btn btn-info" >
-                      <a href={item.url}>View profile</a>
+                    <h4>score :{item.score}</h4>
+                    <h4>Profile url :{item.url}</h4>
+                    <button className="btn btn-info">
+                      <a href={item.url}>Details</a>
                     </button>
                   </div>
                 </div>
