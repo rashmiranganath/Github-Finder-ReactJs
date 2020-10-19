06fadb5b431d7753d42fcb8141e566798988a2c0
rashmiranganath <rashmi18@navgurukul.org>
2020-10-19 19:09:22 +0530

    githubFinder

--- src/components/githubFinder.js
+++ src/components/githubFinder.js
@@ -1,41 +1,38 @@
 import React from "react";
-import axios from "axios";
 import "./githubFinder.css";
-class GithubFinder extends React.Component {
+import { searchUserApi, userRepos } from "../services/service";
+
+class GithubFinder extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
       dropDownValue: "",
       username: "",
       data: [],
-      isLoaded: false
+      isLoaded: false,
+      selectedUserId: "",
+      selectedUserDetails: {},
+      showDetails: false,
     };
   }
   updateInput(input) {
     this.setState({ username: input });
   }
-  searchUserApi(username) {
-    console.log(this.state);
-    console.log(username);
-    axios
-      .get(`https://api.github.com/search/users?q=${username}`)
-      .then(response => {
-        console.log(response.data.items);
-        this.setState({
-          data: response.data.items,
-          isLoaded: true
-        });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
+
+  searchUsers = async (username) => {
+    const response = await searchUserApi(username);
+    this.setState({
+      data: response.data.items,
+      isLoaded: true,
+    });
+  };
+
   dropDownChangeValue(event) {
     this.setState({
-      dropDownValue: event
+      dropDownValue: event,
     });
   }
-  handleSubmit = event => {
+  handleSubmit = (event) => {
     const { data } = this.state;
     const newData = [...data];
     event.preventDefault();
@@ -46,14 +43,14 @@ class GithubFinder extends React.Component {
         console.log(sorted);
         console.log(this.state.dropDownValue);
         this.setState({
-          data: sorted
+          data: sorted,
         });
         break;
       case "Descending":
         var sorted1 = newData.sort((x, y) => (x.login < y.login ? 1 : -1));
         console.log("d", sorted1);
         this.setState({
-          data: sorted1
+          data: sorted1,
         });
         break;
       default:
@@ -61,7 +58,7 @@ class GithubFinder extends React.Component {
     }
   };
   renderNavBar = () => {
-    const { data, username, dropDownValue } = this.state;
+    const { username, dropDownValue } = this.state;
     return (
       <div className="inputBox">
         <div className="sortBox">
@@ -69,7 +66,7 @@ class GithubFinder extends React.Component {
             <select
               name="sort"
               value={dropDownValue}
-              onChange={event => {
+              onChange={(event) => {
                 this.dropDownChangeValue(event.target.value);
               }}
             >
@@ -85,46 +82,102 @@ class GithubFinder extends React.Component {
             required
             placeholder="username"
             value={this.state.username}
-            onChange={e => this.updateInput(e.target.value)}
+            onChange={(e) => this.updateInput(e.target.value)}
           ></input>
         </div>
         <div className="submitBox">
-          <button onClick={() => this.searchUserApi(username)}>search</button>
+          <button onClick={() => this.searchUsers(username)}>search</button>
         </div>
       </div>
     );
   };
+
+  handleDetails = async (id, url, index) => {
+    console.log(id);
+
+    const { selectedUserId, showDetails } = this.state;
+    const resp = await userRepos(url);
+    console.log(resp);
+    this.setState({
+      showDetails: !showDetails,
+      selectedUserId: id,
+      selectedUrl: url,
+      selectedUserDetails: resp.data,
+    });
+    console.log(showDetails);
+  };
+
+  renderDetail = (showDetails) => {
+    const { selectedUserDetails } = this.state;
+
+    const userData = selectedUserDetails;
+    console.log(userData);
+    return (
+      <div className="userData">
+        <h3>Repositories ({userData.length})</h3>
+        {userData.map((repos) => {
+          return (
+            <div className="userRepos" key={repos.id}>
+              <div className="repoInfo">
+                <h2>{repos.name}</h2>
+                <p>language : {repos.language}</p>
+                <p>fork count : {repos.forks_count}</p>
+                <p>followers : {repos.watchers}</p>
+                <p>stars : {repos.stargazers_count}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+
   renderUsers = () => {
-    const { data, username, dropDownValue } = this.state;
+    const { showDetails, data, selectedUserId } = this.state;
     return (
       <div className="users">
-          <ul>
-            {data.map(item => {
-              return (
-                <div className="searchResults" key={item.id}>
-                  <img src={item.avatar_url}></img>
+        <ul>
+          {data.map((item) => {
+            return (
+              <div className="searchResults" key={item.id}>
+                <div className="userInfo">
+                  <div className="imageDiv">
+                    <img src={item.avatar_url} alt="avatar" />
+                  </div>
                   <div className="userDetails">
                     <h3>{item.login}</h3>
                     <h4>score :{item.score}</h4>
                     <h4>Profile url :{item.url}</h4>
-                    <button className="btn btn-info">
-                      <a href={item.url}>Details</a>
-                    </button>
+                    <div>
+                      <button
+                        className="btn btn-info"
+                        onClick={() =>
+                          this.handleDetails(item.id, item.repos_url)
+                        }
+                      >
+                        {selectedUserId === item.id && showDetails
+                          ? "CloseDetails"
+                          : "OpenDetails"}
+                      </button>
+                    </div>
                   </div>
                 </div>
-              );
-            })}
-          </ul>
-        </div>
-    )
-  }
+                {showDetails
+                  ? selectedUserId === item.id && this.renderDetail(item.id)
+                  : ""}
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  };
+
   render() {
     return (
       <div className="mainContainer">
-      <div className="navBar">
-        {this.renderNavBar()}
-      </div>
-      {this.renderUsers()}
+        <div className="navBar">{this.renderNavBar()}</div>
+        {this.renderUsers()}
       </div>
     );
   }
--- src/services/service.js
+++ src/services/service.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-
-
 const searchUserApi = (username) => {
-    console.log(username);
-    return axios
-      .get(`https://api.github.com/search/users?q=${username}`)
+  console.log(username);
+  return axios.get(`https://api.github.com/search/users?q=${username}`);
+};
 
-  }
+const userRepos = (url) => {
+  return axios.get(url);
+};
 
-export {searchUserApi}
+export { searchUserApi, userRepos };
